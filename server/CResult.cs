99a4db4b02c408eb69c96cdd31aaa6169470c818
oyama <ka-ooyama@ous.ac.jp>
2023-09-08 17:22:35 +0900

    add

---
+++ server/CResult.cs
@@ -0,0 +1,55 @@
+﻿using System.Numerics;
+
+namespace reversi
+{
+    internal class CResult
+    {
+        public CResult()
+        { }
+        ~CResult() { }
+
+        public void set(ulong[] board)
+        {
+            int board0 = BitOperations.PopCount(board[0]);
+            int board1 = BitOperations.PopCount(board[1]);
+
+            if (board0 > board1) {
+                win = 1;
+                high = low = Reversi.rows * Reversi.columns - (int)board1;
+            } else if (board0 < board1) {
+                lose = 1;
+                high = low = (int)board0 - Reversi.rows * Reversi.columns;
+            } else {
+                draw = 1;
+                high = low = 0;
+            }
+        }
+
+        public void marge(CResult result)
+        {
+            high = Math.Max(high, result.high);
+            low = Math.Min(low, result.low);
+            win += result.win;
+            lose += result.lose;
+            draw += result.draw;
+        }
+
+        public int winRate(int player)
+        {
+            ulong match = win + lose + draw;
+            int rate = (int)(((double)win + 0.5 * (double)draw) / (double)match * 100.0);
+            return player == 0 ? rate : 100 - rate;
+        }
+
+        void print()
+        {
+            //printf("win(%8d) lose(%8d) draw(%8d) high(%+3d) low(%+3d)\n", win, lose, draw, high, low);
+        }
+
+        int high = int.MinValue;
+        int low = int.MaxValue;
+        ulong win = 0;
+        ulong lose = 0;
+        ulong draw = 0;
+    }
+}
---
+++ server/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace reversi
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            pictureBox1 = new PictureBox();
+            button1 = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = Color.DarkGreen;
+            pictureBox1.Dock = DockStyle.Fill;
+            pictureBox1.Location = new Point(0, 0);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(578, 424);
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            pictureBox1.Paint += pictureBox1_Paint;
+            pictureBox1.MouseDown += pictureBox1_MouseDown;
+            // 
+            // button1
+            // 
+            button1.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            button1.Location = new Point(454, 12);
+            button1.Name = "button1";
+            button1.Size = new Size(112, 34);
+            button1.TabIndex = 1;
+            button1.Text = "はじめから";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(578, 424);
+            Controls.Add(button1);
+            Controls.Add(pictureBox1);
+            Name = "Form1";
+            Text = "Panda";
+            Resize += Form1_Resize;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private Button button1;
+    }
+}
