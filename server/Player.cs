2ec92714e2a6c43de53f090a396152a7d30ee7a6
yuki2 <a22i118bw@ous.jp>
2024-04-25 13:00:42 +0900

    risultで手牌とアタリ牌表示

--- server/Player.cs
+++ server/Player.cs
@@ -163,6 +163,10 @@ namespace server
             _kawa.Draw(g, _id);
             _actionCommand.Draw(g, teban);
         }
+        public void Draw(Graphics g, bool teban,Hai hai)
+        {
+
+        }
 
         public string[] YakuString()
         {
